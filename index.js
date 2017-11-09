6a44e422bc597be9633491922ca0c3260a0ed5b4
JMGallipo <michael.gallipo@gmail.com>
2017-11-09 17:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
-  kittens.slice(0, kittens.length - 1)
+  return [kittens.slice(0, kittens.length - 1)]
 }
